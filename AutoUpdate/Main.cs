4eefcadc23b5067e2c98f2069f8656a129d97bb9
fiass <roma.sokol200219@gmail.com>
2021-12-12 16:44:26 +0500

    AutoUpdateVersa

---
+++ AutoUpdate/Main.cs
@@ -0,0 +1,96 @@
+﻿using Ionic.Zip;
+using MelonLoader;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdate
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Versa Update"; // Name of the Plugin.  (MUST BE SET)
+        public const string Description = "Plugin for Versa Updating"; // Description for the Plugin.  (Set as null if none)
+        public const string Author = "Null"; // Author of the Plugin.  (MUST BE SET)
+        public const string Company = null; // Company that made the Plugin.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Plugin.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Plugin.  (Set as null if none)
+    }
+
+    public class AutoUpdate : MelonPlugin
+    {
+        public override void OnPreInitialization() // Runs before Game Initialization.
+        {
+            Download();
+
+        }
+        internal static string Path = Directory.GetCurrentDirectory();
+        internal static async void Download()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile("https://raw.githubusercontent.com/fiass/Versa/Versa-Data/Loader/Versa-Setup", $@"{Path}\Versa-Setup.zip");
+                    while (client.IsBusy)
+                    {
+                        await Task.Delay(100);
+                    }
+                }
+            }
+            catch { }
+            try
+            {
+                using (ZipFile zip = ZipFile.Read($@"{Path}\Versa-Setup.zip", new ReadOptions { Encoding = Encoding.GetEncoding("cp866") }))
+                {
+                    foreach (ZipEntry eq in zip)
+                    {
+                        eq.ExtractExistingFile = ExtractExistingFileAction.OverwriteSilently;
+                        eq.ExtractWithPassword(Path, new string(CharCrypt.SetupVersa_Crypt.Char_1));
+                    }
+                }
+            }
+            catch { }
+            try
+            {
+                File.Delete($@"{Path}\Versa-Setup.zip");
+            }
+            catch { }
+        }
+        public override void OnApplicationEarlyStart() // Runs after Game Initialization, before OnApplicationStart and (on Il2Cpp games) before Unhollower.
+        {
+        }
+
+        public override void OnApplicationStart() // Runs after Game Initialization.
+        {
+        }
+
+        public override void OnApplicationLateStart() // Runs after OnApplicationStart.
+        {
+        }
+
+        public override void OnUpdate() // Runs once per frame.
+        {
+        }
+
+        public override void OnLateUpdate() // Runs once per frame after OnUpdate and OnFixedUpdate have finished.
+        {
+        }
+
+        public override void OnGUI() // Can run multiple times per frame. Mostly used for Unity's IMGUI.
+        {
+        }
+
+        public override void OnApplicationQuit() // Runs when the Game is told to Close.
+        {
+        }
+
+        public override void OnPreferencesSaved() // Runs when Melon Preferences get saved.
+        {
+        }
+
+        public override void OnPreferencesLoaded() // Runs when Melon Preferences get loaded.
+        {
+        }
+    }
+}
---
+++ AutoUpdate/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(AutoUpdate.BuildInfo.Description)]
+[assembly: AssemblyDescription(AutoUpdate.BuildInfo.Description)]
+[assembly: AssemblyCompany(AutoUpdate.BuildInfo.Company)]
+[assembly: AssemblyProduct(AutoUpdate.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + AutoUpdate.BuildInfo.Author)]
+[assembly: AssemblyTrademark(AutoUpdate.BuildInfo.Company)]
+[assembly: AssemblyVersion(AutoUpdate.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(AutoUpdate.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(AutoUpdate.AutoUpdate), AutoUpdate.BuildInfo.Name, AutoUpdate.BuildInfo.Version, AutoUpdate.BuildInfo.Author, AutoUpdate.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
