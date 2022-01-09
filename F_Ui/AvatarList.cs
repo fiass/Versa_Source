26dcda335da6142f436a1743746b6908ee2ab83d
Kyila <kaithegecko@gmail.com>
2022-01-08 23:14:48 -0500

    There fucking mostly done

--- F_Ui/AvatarList.cs
+++ F_Ui/AvatarList.cs
@@ -1,7 +1,12 @@
-﻿using Il2CppSystem.Runtime.Remoting.Messaging;
+﻿using System;
+using System.IO;
+using Il2CppSystem.Collections.Generic;
+using Il2CppSystem.Runtime.Remoting.Messaging;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 using Versa.F_Core;
+using Versa.F_Output;
 using Versa.F_Ui.Kai;
 using VRC.Core;
 
@@ -9,11 +14,13 @@ namespace Versa.F_Ui
 {
     internal class AvatarList
     {
+        private  static System.Collections.Generic.List<JsonAvatar> Avatars { get; set; }
         private static Text HeaderText { get; set; }
         private static GameObject ListObject { get; set; }
         private static UiAvatarList AviListBase { get; set; }
         internal static void Create()
         {
+            Load();
             ListObject = GameObject.Instantiate(UICache.FavoriteList, UICache.FavoriteList.transform.parent);
             ListObject.name = $"Favorite Versa List";
             ListObject.transform.SetSiblingIndex(UICache.FavoriteList.transform.GetSiblingIndex() + -1);
@@ -25,6 +32,11 @@ namespace Versa.F_Ui
             HeaderText.supportRichText = true;
             HeaderText.text = "<color=#00ccff>Versa Favorites</color>";
         }
+
+        internal static void Render()
+        {
+            
+        }
         
         //TODO:Create button api for lists
         private static void CreateButtons()
@@ -35,14 +47,69 @@ namespace Versa.F_Ui
         
         internal static void Add(ApiAvatar avatar)
         {
-            if (!Database.Avatars.Exists(x => x.id == avatar.id))
-                Database.Avatars.Insert(avatar);
+            Avatars.Add(JsonAvatar.ParseAvatar(avatar));
+            Save();
         }
 
         internal static void Remove(ApiAvatar avatar)
         {
-            if (Database.Avatars.Exists(x => x.id == avatar.id))
-                Database.Avatars.DeleteMany(x => x.id == avatar.id);
+            Avatars.Add(JsonAvatar.ParseAvatar(avatar));
+            Save();
+        }
+
+        internal static void Save()
+        {
+            try
+            {
+                File.WriteAllText("Versa\\Favorites.Versa", JsonConvert.SerializeObject(Avatars));
+            }
+            catch (Exception e)
+            {
+                CustomConsole.Console(false, "Failed to save favorites");
+            }
+        }
+
+        internal static void Load()
+        {
+            try
+            {
+                if(File.Exists("Versa\\Favorites.Versa")) Avatars = JsonAvatar.ParseList(File.ReadAllText("Versa\\Favorites.Versa"));
+                else
+                {
+                    Avatars = new System.Collections.Generic.List<JsonAvatar>() {new JsonAvatar() { }};
+                    File.WriteAllText("Versa\\Favorites.Versa", JsonConvert.SerializeObject(Avatars));
+                }
+            }
+            catch (Exception e)
+            {
+                CustomConsole.Console(true, "Failed to parse favorites");
+            }
+        }
+    }
+    
+    internal class JsonAvatar
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string releaseStatus { get; set; }
+        public string assetURL { get; set; }
+        public string ImageURL { get; set; }
+
+        internal static System.Collections.Generic.List<JsonAvatar> ParseList(string Data)
+        {
+            return JsonConvert.DeserializeObject<System.Collections.Generic.List<JsonAvatar>>(Data);
+        }
+
+        internal static JsonAvatar ParseAvatar(ApiAvatar avatar)
+        {
+            return new JsonAvatar()
+            {
+                id = avatar.id,
+                name = avatar.name,
+                releaseStatus = avatar.releaseStatus,
+                assetURL = avatar.assetUrl,
+                ImageURL = avatar.imageUrl
+            };
         }
     }
 }
